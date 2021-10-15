73e49f0da2697fcc19f72a1e9fbfed61fbbf0c9f
sarabjot294 <sarabot294@gmail.com>
2021-10-15 13:36:27 -0700

    Add project files.

---
+++ OsmDataFromMap/OsmElements.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using JsonSubTypes;
+
+namespace OsmDataFromMap
+{
+    // For More Reference of this class go to https://stackoverflow.com/questions/56284030/deserialize-open-street-map-json-in-c-sharp
+
+    public class OSMdata
+    {
+        public float version { get; set; }
+        public string generator { get; set; }
+        public Osm3s osm3s { get; set; }
+
+        // for arrays or collection this line must be present here
+        [JsonConverter(typeof(JsonSubtypes), "type")]
+        public Element[] elements { get; set; }
+    }
+
+    // no need to inherits from OSMData
+    public class Osm3s
+    {
+        public DateTime timestamp_osm_base { get; set; }
+        public string copyright { get; set; }
+    }
+
+
+    [JsonConverter(typeof(JsonSubtypes), "type")]
+    [JsonSubtypes.KnownSubType(typeof(Node), "node")]
+    [JsonSubtypes.KnownSubType(typeof(Edge), "way")]
+    public abstract class Element : OSMdata
+    {
+        public abstract string type { get; }
+    }
+
+    public class Node : Element
+    {
+        public override string type { get; } = "node";
+        public long id { get; set; }
+        public float lat { get; set; }
+        public float lon { get; set; }
+        public NodeTags tags { get; set; }
+    }
+
+    public class NodeTags
+    {
+        public string highway { get; set; }
+        public string _ref { get; set; }
+    }
+
+    public class Edge : Element
+    {
+        public override string type { get; } = "way";
+        public long id { get; set; }
+        public long[] nodes { get; set; }
+        public EdgeTags tags { get; set; }
+    }
+
+    public class EdgeTags
+    {
+        public string highway { get; set; }
+        public string name { get; set; }
+        public string cfcc { get; set; }
+        public string county { get; set; }
+        public string oneway { get; set; }
+    }
+}
---
+++ OsmDataFromMap/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Drawing;
+
+namespace OsmDataFromMap
+
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            //To Use Coordinate and a zoom level, read more at https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Zoom_levels
+            //Using Boundry Box
+            await SendRequest(47.389704284254435, -122.05152509779643, 47.391717802883036, -122.04711567641095);
+        }
+
+        private static async Task SendRequest(double min_lat, double min_long, double max_lat, double max_lon)
+        {
+            var URL = string.Format("http://overpass-api.de/api/interpreter?data=[out:json];(node({0},{1},{2},{3});<;);out meta;",min_lat, min_long, max_lat, max_lon);
+            Console.WriteLine(URL);
+            var stringTask = client.GetStringAsync(URL);
+            var msg = await stringTask;
+            var json = JsonConvert.DeserializeObject<OSMdata>(msg);
+            Console.WriteLine(msg);
+            Console.WriteLine(json.elements.Length);
+        }
+    }
+}
